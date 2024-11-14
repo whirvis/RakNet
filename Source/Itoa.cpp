b9d92f9a937267e97abc3b15a58234d701121331
Trent Summerlin <whirvis@ardenus.org>
2024-11-14 17:46:29 -0600

    Tidy up Itoa() function

--- Source/Itoa.cpp
+++ Source/Itoa.cpp
@@ -1,62 +1,59 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
+#include "Itoa.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+static const char *NUMERIC_CHARS = "0123456789abcdef";
 
-// Fast itoa from http://www.jb.man.ac.uk/~slowe/cpp/itoa.html for Linux since it seems like Linux doesn't support this function.
-// I modified it to remove the std dependencies.
-char* Itoa( int value, char* result, int base )
- {
-	// check that the base if valid
-	if (base < 2 || base > 16) { *result = 0; return result; }
-	char* out = result;
-	int quotient = value;
+/*
+ * Fast implementation of `itoa`, taken from Manchester University
+ * for Linux; since it seems Linux doesn't support this function.
+ * It has been modified so as not to have any dependences on the C
+ * standard library.
+ */
+char *
+Itoa(int value, char *result, int base) {
+    /* terminate if base is unsupported */
+    if (base < 2 || base > 16) {
+        *result = '\0';
+        return result;
+    }
 
-	int absQModB;
+    char *out = result;
+    int quotient = value;
 
-	do {
-		// KevinJ - get rid of this dependency
-		//*out = "0123456789abcdef"[ std::abs( quotient % base ) ];
-		absQModB=quotient % base;
-		if (absQModB < 0)
-			absQModB=-absQModB;
-		*out = "0123456789abcdef"[ absQModB ];
-		++out;
-		quotient /= base;
-	} while ( quotient );
+    int charIndex;
+    do {
+        charIndex = quotient % base;
+        if (charIndex < 0) {
+            charIndex = -charIndex;
+        }
+        *out++ = NUMERIC_CHARS[charIndex];
+        quotient /= base;
+    } while (quotient);
 
-	// Only apply negative sign for base 10
-	if ( value < 0 && base == 10) *out++ = '-';
-
-	// KevinJ - get rid of this dependency
-	// std::reverse( result, out );
-	*out = 0;
-
-	// KevinJ - My own reverse code
+    /* only apply negative sign for decimal */
+    if (value < 0 && base == 10) {
+        *out++ = '-';
+    }
+    
+    /* terminate the string */
+    *out = '\0';
+    
+    /* lastly reverse the string */
     char *start = result;
-	char temp;
-	out--;
-	while (start < out)
-	{
-		temp=*start;
-		*start=*out;
-		*out=temp;
-		start++;
-		out--;
-	}
+    out--; /* don't reverse the terminator */
+    char temp;
+    while (start < out) {
+        temp = *start;
+        *start++ = *out;
+        *out-- = temp;
+    }
 
-	return result;
+    return result;
 }
-
-#ifdef __cplusplus
-}
-#endif
