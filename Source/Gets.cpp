dc0b73bd82e994588b9541c95e683ccef0e6b605
Trent Summerlin <whirvis@ardenus.org>
2024-11-14 17:58:59 -0600

    Tidy up Gets() function

--- Source/Gets.cpp
+++ Source/Gets.cpp
@@ -1,35 +1,40 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
+#include "Gets.h"
 
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+char *
+Gets (char *str, int num) {
+    std::fgets(str, num, stdin);
+    
+    char c = str[0];
+    if (c == '\n' || c == '\r') {
+        str[0] = '\0';
+    }
 
-char * Gets ( char * str, int num )
-{
-	fgets(str, num, stdin);
-	if (str[0]=='\n' || str[0]=='\r')
-		str[0]=0;
+    size_t len = std::strlen(str);
 
-	size_t len=strlen(str);
-	if (len>0 && (str[len-1]=='\n' || str[len-1]=='\r'))
-		str[len-1]=0;
-	if (len>1 && (str[len-2]=='\n' || str[len-2]=='\r'))
-		str[len-2]=0;
+    if (len > 0) {
+        char d = str[len - 1];
+        if((d == '\n' || d == '\r')) {
+            str[len - 1] = '\0';
+        }
+    }
 
-	return str;
+    if (len > 1) {
+        char d = str[len - 2];
+        if((d == '\n' || d == '\r')) {
+            str[len - 2] = '\0';
+        }
+    }
+
+    return str;
 }
-
-#ifdef __cplusplus
-}
-#endif
--- Source/Gets.h
+++ Source/Gets.h
@@ -1,24 +1,23 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-#ifndef __GETS__H_
-#define __GETS__H_
+#ifndef RAKNET_GETS_H
+#define RAKNET_GETS_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-char * Gets ( char * str, int num );
+char *
+Gets(char *str, int num);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* RAKNET_GETS_H */
