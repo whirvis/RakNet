c189f5078254c86b4aed994ecdf86770bf4eed3c
Trent Summerlin <whirvis@ardenus.org>
2024-11-14 01:18:19 -0600

    Tidy up native feature include headers

--- Source/NativeFeatureIncludesOverrides.h
+++ Source/NativeFeatureIncludesOverrides.h
@@ -1,19 +1,57 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-// USER EDITABLE FILE
-// See NativeFeatureIncludes.h
+// USER EDITABLE FILE, CHANGES WILL BE KEPT WHEN UPDATING RAKNET.
+// See NativeFeatureIncludes.h for more information.
 
-#ifndef __NATIVE_FEATURE_INCLDUES_OVERRIDES_H
-#define __NATIVE_FEATURE_INCLDUES_OVERRIDES_H
+#ifndef RAKNET_NATIVE_FEATURE_INCLDUES_OVERRIDES_H
+#define RAKNET_NATIVE_FEATURE_INCLDUES_OVERRIDES_H
 
-//#define LIBCAT_SECURITY 1
+// #define LIBCAT_SECURITY 1
 
-#endif
+/*
+ * Uncomment the defines bellow corresponding to the plugins which you
+ * don't want built into the static library or DLL. These are not all of
+ * the plugins, only those that are in the core library.
+ */
+
+/* other plugins are located in DependentExtensions */
+
+// #define _RAKNET_SUPPORT_ConnectionGraph2       0
+// #define _RAKNET_SUPPORT_DirectoryDeltaTransfer 0
+// #define _RAKNET_SUPPORT_FileListTransfer       0
+// #define _RAKNET_SUPPORT_FullyConnectedMesh2    0
+// #define _RAKNET_SUPPORT_MessageFilter          0
+// #define _RAKNET_SUPPORT_NatPunchthroughClient  0
+// #define _RAKNET_SUPPORT_NatPunchthroughServer  0
+// #define _RAKNET_SUPPORT_NatTypeDetectionClient 0
+// #define _RAKNET_SUPPORT_NatTypeDetectionServer 0
+// #define _RAKNET_SUPPORT_PacketLogger           0
+// #define _RAKNET_SUPPORT_ReadyEvent             0
+// #define _RAKNET_SUPPORT_ReplicaManager3        0
+// #define _RAKNET_SUPPORT_Router2                0
+// #define _RAKNET_SUPPORT_RPC4Plugin             0
+// #define _RAKNET_SUPPORT_TeamBalancer           0
+// #define _RAKNET_SUPPORT_TeamManager            0
+// #define _RAKNET_SUPPORT_UDPProxyClient         0
+// #define _RAKNET_SUPPORT_UDPProxyCoordinator    0
+// #define _RAKNET_SUPPORT_UDPProxyServer         0
+// #define _RAKNET_SUPPORT_ConsoleServer          0
+// #define _RAKNET_SUPPORT_RakNetTransport        0
+// #define _RAKNET_SUPPORT_TelnetTransport        0
+// #define _RAKNET_SUPPORT_TCPInterface           0
+// #define _RAKNET_SUPPORT_LogCommandParser       0
+// #define _RAKNET_SUPPORT_RakNetCommandParser    0
+// #define _RAKNET_SUPPORT_EmailSender            0
+// #define _RAKNET_SUPPORT_HTTPConnection         0
+// #define _RAKNET_SUPPORT_HTTPConnection2        0
+// #define _RAKNET_SUPPORT_PacketizedTCP          0
+// #define _RAKNET_SUPPORT_TwoWayAuthentication   0
+
+#endif /* RAKNET_NATIVE_FEATURE_INCLDUES_OVERRIDES_H */
