829c92f8c0854a861ce01f205f991174c72c194f
Trent Summerlin <whirvis@ardenus.org>
2024-11-14 22:47:03 -0600

    Tidy up files for getche()

--- Source/Getche.cpp
+++ Source/Getche.cpp
@@ -1,35 +1,26 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-#if   defined(_WIN32)
-#include <conio.h> /* getche() */
-#elif  defined(__S3E__)
-
-#else
-
 #include "Getche.h"
 
-char getche()
-{
+#if defined(_WIN32)
+    /* getche() already included */
+#else
+    char
+    getche() {
+        struct termios oldt;
+        tcgetattr(STDIN_FILENO, &oldt);
+        struct termios newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
 
-
-  struct termios oldt,
-                 newt;
-  char            ch;
-  tcgetattr( STDIN_FILENO, &oldt );
-  newt = oldt;
-  newt.c_lflag &= ~( ICANON | ECHO );
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-  ch = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-  return ch;
-
-} 
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        char ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return ch;
+    }
 #endif
--- Source/Getche.h
+++ Source/Getche.h
@@ -1,19 +1,17 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-#if   defined(_WIN32)
-#include <conio.h> /* getche() */
-
+#if defined(_WIN32)
+    #include <conio.h> /* getche() */
 #else
-#include <termios.h>
-#include <stdio.h>
-#include <unistd.h>
-char getche();
-#endif 
+    #include <termios.h>
+    #include <stdio.h>
+    #include <unistd.h>
+
+    char getche();
+#endif
