4b4844c95a2f6d9f383f7b78e120887059c2327a
Trent Summerlin <whirvis@ardenus.org>
2024-11-14 18:25:31 -0600

    Tidy up CheckSum class

--- Source/CheckSum.cpp
+++ Source/CheckSum.cpp
@@ -1,107 +1,67 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-/**
-* @file 
-* @brief CheckSum implementation from http://www.flounder.com/checksum.htm
-* 
-*/
 #include "CheckSum.h"
 
-/****************************************************************************
-*        CheckSum::add
-* Inputs:
-*   unsigned int d: word to add
-* Result: void
-* 
-* Effect: 
-*   Adds the bytes of the unsigned int to the CheckSum
-****************************************************************************/
+inline
+CheckSum::CheckSum() {
+    this->Clear();
+}
 
-void CheckSum::Add ( unsigned int value )
-{
-	union
-	{
-		unsigned int value;
-		unsigned char bytes[ 4 ];
-	}
+inline unsigned int
+CheckSum::Get() {
+    return this->sum;
+}
 
-	data;
-	data.value = value;
+void
+CheckSum::Add(unsigned int value) {
+    union {
+        unsigned int value;
+        unsigned char bytes[4];
+    } data = { 0 };
 
-	for ( unsigned int i = 0; i < sizeof( data.bytes ); i++ )
-		Add ( data.bytes[ i ] )
+    data.value = value;
+    for (unsigned int i = 0; i < sizeof(data.bytes); i++) {
+        this->Add(data.bytes[i]);
+    }
+}
 
-		;
-} // CheckSum::add(unsigned int)
+void
+CheckSum::Add(unsigned short value) {
+    union {
+        unsigned short value;
+        unsigned char bytes[2];
+    } data = { 0 };
 
-/****************************************************************************
-*       CheckSum::add
-* Inputs:
-*   unsigned short value:
-* Result: void
-* 
-* Effect: 
-*   Adds the bytes of the unsigned short value to the CheckSum
-****************************************************************************/
+    data.value = value;
+    for (unsigned int i = 0; i < sizeof(data.bytes); i++) {
+        this->Add (data.bytes[i]);
+    }
+}
 
-void CheckSum::Add ( unsigned short value )
-{
-	union
-	{
-		unsigned short value;
-		unsigned char bytes[ 2 ];
-	}
+void
+CheckSum::Add(unsigned char value) {
+    unsigned char cipher = (unsigned char) (value ^ (r >> 8));
+    this->r = (cipher + r) * c1 + c2;
+    this->sum += cipher;
+}
 
-	data;
-	data.value = value;
+void
+CheckSum::Add (unsigned char *b, unsigned int length) {
+    for (unsigned int i = 0; i < length; i++) {
+        this->Add(b[i]);
+    }
+}
 
-	for ( unsigned int i = 0; i < sizeof( data.bytes ); i++ )
-		Add ( data.bytes[ i ] )
-
-		;
-} // CheckSum::add(unsigned short)
-
-/****************************************************************************
-*       CheckSum::add
-* Inputs:
-*   unsigned char value:
-* Result: void
-* 
-* Effect: 
-*   Adds the byte to the CheckSum
-****************************************************************************/
-
-void CheckSum::Add ( unsigned char value )
-{
-	unsigned char cipher = (unsigned char)( value ^ ( r >> 8 ) );
-	r = ( cipher + r ) * c1 + c2;
-	sum += cipher;
-} // CheckSum::add(unsigned char)
-
-
-/****************************************************************************
-*       CheckSum::add
-* Inputs:
-*   LPunsigned char b: pointer to byte array
-*   unsigned int length: count
-* Result: void
-* 
-* Effect: 
-*   Adds the bytes to the CheckSum
-****************************************************************************/
-
-void CheckSum::Add ( unsigned char *b, unsigned int length )
-{
-	for ( unsigned int i = 0; i < length; i++ )
-		Add ( b[ i ] )
-
-		;
-} // CheckSum::add(LPunsigned char, unsigned int)
+inline void
+CheckSum::Clear() {
+   this->r = 55665;
+   this->c1 = 52845;
+   this->c2 = 22719;
+   this->sum = 0;
+}
--- Source/CheckSum.h
+++ Source/CheckSum.h
@@ -1,63 +1,45 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-///
-/// \file CheckSum.cpp
-/// \brief [Internal] CheckSum implementation from http://www.flounder.com/checksum.htm
-///
+/*!
+ * @file ChecSum.cpp
+ * 
+ * @brief [Internal] CheckSum implementation from http://www.flounder.com/checksum.htm
+ */
 
-#ifndef __CHECKSUM_H
-#define __CHECKSUM_H
+#ifndef RAKNET_CHECKSUM_H
+#define RAKNET_CHECKSUM_H
 
 #include "RakMemoryOverride.h"
 
-/// Generates and validates checksums
-class CheckSum
-{
+class CheckSum {
+
+private:
+
+    unsigned short r;
+    unsigned short c1;
+    unsigned short c2;
+    unsigned int sum;
 
 public:
-	
- /// Default constructor
-	
-	CheckSum()
-	{
-		Clear();
-	}
-	
-	void Clear()
-	{
-		sum = 0;
-		r = 55665;
-		c1 = 52845;
-		c2 = 22719;
-	}
-	
-	void Add ( unsigned int w );
-	
-	
-	void Add ( unsigned short w );
-	
-	void Add ( unsigned char* b, unsigned int length );
-	
-	void Add ( unsigned char b );
-	
-	unsigned int Get ()
-	{
-		return sum;
-	}
-	
-protected:
-	unsigned short r;
-	unsigned short c1;
-	unsigned short c2;
-	unsigned int sum;
+
+    CheckSum();
+
+    unsigned int Get();
+    
+    void Add(unsigned int   w);
+    void Add(unsigned short w);
+    void Add(unsigned char  b);
+    void Add(unsigned char *b, unsigned int length);
+
+    void Clear();
+
 };
 
-#endif
+#endif /* RAKNET_CHECKSUM_H */
