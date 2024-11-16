0d5093fc50533ba875795ce7767154a14447b599
Trent Summerlin <whirvis@ardenus.org>
2024-11-16 11:32:50 -0600

    Tidy up RakSleep.h, RakSleep.cpp

--- Source/RakSleep.h
+++ Source/RakSleep.h
@@ -1,18 +1,16 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-#ifndef __RAK_SLEEP_H
-#define __RAK_SLEEP_H
+#ifndef RAKNET_SLEEP_H
+#define RAKNET_SLEEP_H
 
 #include "Export.h"
 
-void RAK_DLL_EXPORT RakSleep(unsigned int ms);
+RAK_DLL_EXPORT void RakSleep(unsigned int ms);
 
-#endif
+#endif /* RAKNET_SLEEP_H */
