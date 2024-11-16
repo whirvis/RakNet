b4e40c67d948981076dafbab6998571c0403658a
Trent Summerlin <whirvis@ardenus.org>
2024-11-16 15:52:16 -0600

    Tidy up RakAssert.h

--- Source/RakAssert.h
+++ Source/RakAssert.h
@@ -1,12 +1,16 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
+#ifndef RAKNET_ASSERT_H
+#define RAKNET_ASSERT_H
 
 #include <assert.h>
+
 #include "RakNetDefines.h"
+
+#endif /* RAKNET_ASSERT_H */
