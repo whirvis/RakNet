790858c019251233db683cf561c33cc6473d6c68
Trent Summerlin <whirvis@ardenus.org>
2024-11-16 12:46:46 -0600

    Tidy up SuperFastHash.h, SuperFastHash.cpp

--- Source/SuperFastHash.cpp
+++ Source/SuperFastHash.cpp
@@ -1,129 +1,154 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
 #include "SuperFastHash.h"
-#include "NativeTypes.h"
+
 #include <stdlib.h>
 
-#if !defined(_WIN32)
-#include <stdint.h>
-#endif
+#include "NativeTypes.h"
 
-#undef get16bits
+#define INCREMENTAL_READ_BLOCK 65536
 
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
-  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
-#define get16bits(d) (*((const uint16_t *) (d)))
+#if defined(_MSC_VER)                               \
+        || (defined(__GNUC__) && defined(__i386__)) \
+        || defined(__BORLANDC__)                    \
+        || defined(__TURBOC__)                      \
+        || defined(__WATCOMC__)
+    #define get16bits(_i) (*((const uint16_t *) (_i)))
 #else
-#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
-	+(uint32_t)(((const uint8_t *)(d))[0]) )
+    #define get16bits(_i)                                   \
+        ( (((uint32_t) (((const uint8_t *) (_i))[1])) << 8) \
+        +  ((uint32_t) (((const uint8_t *) (_i))[0]))       )
 #endif
 
-static const int INCREMENTAL_READ_BLOCK=65536;
+uint32_t
+SuperFastHash(const char *data, int length) {
+    unsigned int lastHash = length;
 
-uint32_t SuperFastHash (const char * data, int length)
-{
-	// All this is necessary or the hash does not match SuperFastHashIncremental
-	int bytesRemaining=length;
-	unsigned int lastHash = length;
-	int offset=0;
-	while (bytesRemaining>=INCREMENTAL_READ_BLOCK)
-	{
-		lastHash=SuperFastHashIncremental (data+offset, INCREMENTAL_READ_BLOCK, lastHash );
-		bytesRemaining-=INCREMENTAL_READ_BLOCK;
-		offset+=INCREMENTAL_READ_BLOCK;
-	}
-	if (bytesRemaining>0)
-	{
-		lastHash=SuperFastHashIncremental (data+offset, bytesRemaining, lastHash );
-	}
-	return lastHash;
+    /*
+     * This implementation is necessary to ensure the produced hash is
+     * identicial to the one produced by SuperFastHashIncremental.
+     */
 
-//	return SuperFastHashIncremental(data,len,len);
-}
-uint32_t SuperFastHashIncremental (const char * data, int len, unsigned int lastHash )
-{
-	uint32_t hash = (uint32_t) lastHash;
-	uint32_t tmp;
-	int rem;
+    int remaining = length;
+    int offset = 0;
+    while (remaining >= INCREMENTAL_READ_BLOCK) {
+        lastHash = SuperFastHashIncremental(data + offset,
+            INCREMENTAL_READ_BLOCK, lastHash);
+        remaining -= INCREMENTAL_READ_BLOCK;
+        offset += INCREMENTAL_READ_BLOCK;
+    }
 
-	if (len <= 0 || data == NULL) return 0;
+    if (remaining > 0) {
+        lastHash = SuperFastHashIncremental(data + offset,
+            remaining, lastHash);
+    }
 
-	rem = len & 3;
-	len >>= 2;
-
-	/* Main loop */
-	for (;len > 0; len--) {
-		hash  += get16bits (data);
-		tmp    = (get16bits (data+2) << 11) ^ hash;
-		hash   = (hash << 16) ^ tmp;
-		data  += 2*sizeof (uint16_t);
-		hash  += hash >> 11;
-	}
-
-	/* Handle end cases */
-	switch (rem) {
-		case 3: hash += get16bits (data);
-			hash ^= hash << 16;
-			hash ^= data[sizeof (uint16_t)] << 18;
-			hash += hash >> 11;
-			break;
-		case 2: hash += get16bits (data);
-			hash ^= hash << 11;
-			hash += hash >> 17;
-			break;
-		case 1: hash += *data;
-			hash ^= hash << 10;
-			hash += hash >> 1;
-	}
-
-	/* Force "avalanching" of final 127 bits */
-	hash ^= hash << 3;
-	hash += hash >> 5;
-	hash ^= hash << 4;
-	hash += hash >> 17;
-	hash ^= hash << 25;
-	hash += hash >> 6;
-
-	return (uint32_t) hash;
-
+    return lastHash;
 }
 
-uint32_t SuperFastHashFile (const char * filename)
-{
-	FILE *fp = fopen(filename, "rb");
-	if (fp==0)
-		return 0;
-	uint32_t hash = SuperFastHashFilePtr(fp);
-	fclose(fp);
-	return hash;
+uint32_t
+SuperFastHashIncremental(const char *data, int length, unsigned int lastHash) {
+    if (!data || length <= 0) {
+        return 0;
+    }
+
+    uint32_t hash = lastHash;
+    int remainder = length & 3;
+    length >>= 2;
+
+    uint32_t temp;
+    while (length > 0) {
+        hash += get16bits(data);
+        temp = (get16bits(data + 2) << 11) ^ hash;
+        hash = (hash << 16) ^ temp;
+        hash += hash >> 11;
+        
+        data  += 2 * sizeof(uint16_t);
+        length -= 1;
+    }
+
+    switch (remainder) {
+    case 3:
+        hash += get16bits(data);
+        hash ^= hash << 16;
+        hash ^= data[sizeof(uint16_t)] << 18;
+        hash += hash >> 11;
+        break;
+    case 2:
+        hash += get16bits(data);
+        hash ^= hash << 11;
+        hash += hash >> 17;
+        break;
+    case 1:
+        hash += *data;
+        hash ^= hash << 10;
+        hash += hash >> 1;
+        break;
+    }
+
+    /* force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+
+    return hash;
 }
 
-uint32_t SuperFastHashFilePtr (FILE *fp)
-{
-	fseek(fp, 0, SEEK_END);
-	int length = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	int bytesRemaining=length;
-	unsigned int lastHash = length;
-	char readBlock[INCREMENTAL_READ_BLOCK];
-	while (bytesRemaining>=(int) sizeof(readBlock))
-	{
-		fread(readBlock, sizeof(readBlock), 1, fp);
-		lastHash=SuperFastHashIncremental (readBlock, (int) sizeof(readBlock), lastHash );
-		bytesRemaining-=(int) sizeof(readBlock);
-	}
-	if (bytesRemaining>0)
-	{
-		fread(readBlock, bytesRemaining, 1, fp);
-		lastHash=SuperFastHashIncremental (readBlock, bytesRemaining, lastHash );
-	}
-	return lastHash;
+uint32_t
+SuperFastHashFile(const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        return 0;
+    }
+
+    uint32_t hash = SuperFastHashFilePtr(file);
+    fclose(file);
+    return hash;
+}
+
+uint32_t
+SuperFastHashFilePtr(FILE *fp) {
+    /*
+     * Use the heap instead of the stack here, this
+     * much data is admittedly quite a lot to place
+     * on the stack all at once.
+     */
+    char *readBlock = new char[INCREMENTAL_READ_BLOCK];
+
+    /*
+     * TODO: USING int HERE IS A BUG BUT I CAN'T CHANGE IT
+     *  OTHERWISE THIS FUNCTION WILL GIVE DIFFERENT OUTPUTS
+     *  FROM THE ORIGINAL VERSIONFOR THE SAME INTPUTS.
+     */
+    fseek(fp, 0, SEEK_END);
+    int length = ftell(fp); /* <- BAD :( */
+    fseek(fp, 0, SEEK_SET);
+
+    unsigned int lastHash = length;
+    long remaining = length;
+
+    while (remaining >= sizeof(readBlock)) {
+        fread(readBlock, sizeof(readBlock), 1, fp);
+        lastHash = SuperFastHashIncremental(readBlock,
+            sizeof(readBlock), lastHash);
+        remaining -= sizeof(readBlock);
+    }
+
+    if (remaining > 0) {
+        fread(readBlock, remaining, 1, fp);
+        lastHash = SuperFastHashIncremental(readBlock,
+            remaining, lastHash);
+    }
+
+    delete[] readBlock;
+    return lastHash;
 }
--- Source/SuperFastHash.h
+++ Source/SuperFastHash.h
@@ -1,27 +1,31 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-#ifndef __SUPER_FAST_HASH_H
-#define __SUPER_FAST_HASH_H
+#ifndef RAKNET_SUPER_FAST_HASH_H
+#define RAKNET_SUPER_FAST_HASH_H
 
 #include <stdio.h>
+
 #include "NativeTypes.h"
 
-// From http://www.azillionmonkeys.com/qed/hash.html
-// Author of main code is Paul Hsieh 
-// I just added some convenience functions
-// Also note http://burtleburtle.net/bob/hash/doobs.html, which shows that this is 20% faster than the one on that page but has more collisions
+/*
+ * Taken from: http://www.azillionmonkeys.com/qed/hash.html
+ * The author of the main code is Paul Hsieh.
+ * Some convenience functions have been added.
+ * 
+ * Also note http://burtleburtle.net/bob/hash/doobs.html, which
+ * shows that this is 20% faster than the one on that page but has
+ * more collisions.
+ */
 
-uint32_t SuperFastHash (const char * data, int length);
-uint32_t SuperFastHashIncremental (const char * data, int len, unsigned int lastHash );
-uint32_t SuperFastHashFile (const char * filename);
-uint32_t SuperFastHashFilePtr (FILE *fp);
+uint32_t SuperFastHash(const char *data, int length);
+uint32_t SuperFastHashIncremental(const char *data, int length, unsigned int lastHash);
+uint32_t SuperFastHashFile(const char *filename);
+uint32_t SuperFastHashFilePtr(FILE *fp);
 
-#endif
+#endif /* RAKNET_SUPER_FAST_HASH_H */
