08689a945d7b52774594bcd944a139131dcb2895
Trent Summerlin <whirvis@ardenus.org>
2024-11-15 23:08:35 -0600

    Tidy up Base64Encoder.h and Base64Encoder.cpp

--- Source/Base64Encoder.cpp
+++ Source/Base64Encoder.cpp
@@ -1,95 +1,130 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
 #include "Base64Encoder.h"
+
 #include "RakMemoryOverride.h"
 
-const char *Base64Map(void) {return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";}
-const char *base64Map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const char *base64Map =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-// 3/17/2013 must be unsigned char or else it will use negative indices
-int Base64Encoding(const unsigned char *inputData, int dataLength, char *outputData)
-{
-	// http://en.wikipedia.org/wiki/Base64
-
-	int outputOffset, charCount;
-	int write3Count;
-	outputOffset=0;
-	charCount=0;
-	int j;
-
-	write3Count=dataLength/3;
-	for (j=0; j < write3Count; j++)
-	{
-		// 6 leftmost bits from first byte, shifted to bits 7,8 are 0
-		outputData[outputOffset++]=base64Map[inputData[j*3+0] >> 2];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// Remaining 2 bits from first byte, placed in position, and 4 high bits from the second byte, masked to ignore bits 7,8
-		outputData[outputOffset++]=base64Map[((inputData[j*3+0] << 4) | (inputData[j*3+1] >> 4)) & 63];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// 4 low bits from the second byte and the two high bits from the third byte, masked to ignore bits 7,8
-		outputData[outputOffset++]=base64Map[((inputData[j*3+1] << 2) | (inputData[j*3+2] >> 6)) & 63]; // Third 6 bits
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// Last 6 bits from the third byte, masked to ignore bits 7,8
-		outputData[outputOffset++]=base64Map[inputData[j*3+2] & 63];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-	}
-
-	if (dataLength % 3==1)
-	{
-		// One input byte remaining
-		outputData[outputOffset++]=base64Map[inputData[j*3+0] >> 2];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// Remaining 2 bits from first byte, placed in position, and 4 high bits from the second byte, masked to ignore bits 7,8
-		outputData[outputOffset++]=base64Map[((inputData[j*3+0] << 4) | (inputData[j*3+1] >> 4)) & 63];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// Pad with two equals
-		outputData[outputOffset++]='=';
-		outputData[outputOffset++]='=';
-	}
-	else if (dataLength % 3==2)
-	{
-		// Two input bytes remaining
-
-		// 6 leftmost bits from first byte, shifted to bits 7,8 are 0
-		outputData[outputOffset++]=base64Map[inputData[j*3+0] >> 2];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// Remaining 2 bits from first byte, placed in position, and 4 high bits from the second byte, masked to ignore bits 7,8
-		outputData[outputOffset++]=base64Map[((inputData[j*3+0] << 4) | (inputData[j*3+1] >> 4)) & 63];
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// 4 low bits from the second byte, followed by 00
-		outputData[outputOffset++]=base64Map[(inputData[j*3+1] << 2) & 63]; // Third 6 bits
-		if ((++charCount % 76)==0) {outputData[outputOffset++]='\r'; outputData[outputOffset++]='\n'; charCount=0;}
-
-		// Pad with one equal
-		outputData[outputOffset++]='=';
-		//outputData[outputOffset++]='=';
-	}
-
-	// Append \r\n
-	outputData[outputOffset++]='\r';
-	outputData[outputOffset++]='\n';
-	outputData[outputOffset]=0;
-
-	return outputOffset;
+const char *
+Base64Map() {
+    return base64Map;
 }
 
-int Base64Encoding(const unsigned char *inputData, int dataLength, char **outputData)
-{
-	*outputData = (char*) rakMalloc_Ex(dataLength * 2 + 6, _FILE_AND_LINE_);
-	return Base64Encoding(inputData, dataLength, *outputData);
+int
+Base64Encoding(const unsigned char *input, int inputLength, char *output) {
+    int offset = 0;
+    int charCount = 0;
+
+    int i = 0;
+    int tripletCount = inputLength / 3;
+    while (i < tripletCount) {
+        /* Royal Dutch Airlines mentioned ?!?!?! */
+        unsigned char k = input[i * 3 + 0];
+        unsigned char l = input[i * 3 + 1];
+        unsigned char m = input[i * 3 + 2];
+
+        output[offset++] = base64Map[k >> 2];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = base64Map[((k << 4) | (l >> 4)) & 63];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = base64Map[((l << 2) | (m >> 6)) & 63];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = base64Map[m & 63];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        i += 1;
+    }
+
+    if (inputLength % 3 == 1) {
+        unsigned char k = input[i * 3 + 0];
+        unsigned char l = input[i * 3 + 1];
+
+        output[offset++]=base64Map[k >> 2];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++]=base64Map[((k << 4) | (l >> 4)) & 63];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = '=';
+        output[offset++] = '=';
+    } else if (inputLength % 3 == 2) {
+        unsigned char k = input[i * 3 + 0];
+        unsigned char l = input[i * 3 + 1];
+
+        output[offset++] = base64Map[k >> 2];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = base64Map[((k << 4) | (l >> 4)) & 63];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = base64Map[(l << 2) & 63];
+        if ((++charCount % 76) == 0) {
+            output[offset++] = '\r';
+            output[offset++] = '\n';
+            charCount = 0;
+        }
+
+        output[offset++] = '=';
+    }
+
+    output[offset++] = '\r';
+    output[offset++] = '\n';
+    output[offset] = '\0';
+
+    return offset;
+}
+
+int
+Base64Encoding(const unsigned char *input, int inputLength, char **output) {
+    /*
+     * TODO: This is bad, this doesn't check if the allocation was
+     *  successful before attempting to write to it. Does rakMalloc_Ex
+     *  terminate the program if it fails to allocate? If so, then
+     *  this isn't really an issue.
+     */
+    *output = (char *) rakMalloc_Ex(inputLength * 2 + 6, _FILE_AND_LINE_);
+    return Base64Encoding(input, inputLength, *output);
 }
--- Source/Base64Encoder.h
+++ Source/Base64Encoder.h
@@ -1,26 +1,64 @@
 /*
- *  Copyright (c) 2014, Oculus VR, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014, Oculus VR, Inc.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
-
-#ifndef __BASE_64_ENCODER_H
-#define __BASE_64_ENCODER_H
+#ifndef RAKNET_BASE_64_ENCODER_H
+#define RAKNET_BASE_64_ENCODER_H
 
 #include "Export.h"
 
+#ifdef __cplusplus
 extern "C" {
-/// \brief Returns how many bytes were written.
-// outputData should be at least the size of inputData * 2 + 6
-int Base64Encoding(const unsigned char *inputData, int dataLength, char *outputData);
-}
-
-extern "C" {
-const char *Base64Map(void);
-}
-
 #endif
+
+/*!
+ * @brief
+ * Returns the map used for Base64 encoding.
+ */
+const char *Base64Map();
+
+/*!
+ * @brief
+ * Converts a byte array to a Base64 encoded string.
+ * 
+ * @warn The `output` buffer must have a size of at least
+ *       `(inputLength * 2) + 6`.
+ * 
+ * @param[in] input       The byte array to encode.
+ * @param[in] inputLength The size of `input` in bytes.
+ * @param[in] output      Where to write the string.
+ * 
+ * @return How many bytes were written to `output`.
+ */
+int Base64Encoding(const unsigned char *input, int inputLength, char *output);
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * The function below is overloaded, so it must not go inside the above
+ * extern "C" block (functions with C linkage cannot be overloaded).
+ */
+
+/*!
+ * @brief
+ * Converts a byte array to a Base64 encoded string.
+ * 
+ * This version of the method allocates the necessary memory to store the
+ * encoded string. The caller is responsible for freeing this string once
+ * once they are through with it.
+ * 
+ * @param[in]  input       The byte array to encode.
+ * @param[in]  inputLength The size of `input` in bytes.
+ * @param[out] output      Where to write the pointer to the string.
+ * 
+ * @return How many bytes were written to `output`.
+ */
+int Base64Encoding(const unsigned char *input, int inputLength, char **output);
+
+#endif /* RAKNET_BASE_64_ENCODER_H */
